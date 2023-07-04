3e33afcb91c879387117b507ead3cf3efe9f9fc7
Frajdi <frajdi_malaj.fshnstudent@unitir.edu.al>
2023-07-04 18:19:02 +0000

    table added manager part

--- hasura-badges/migrations/default/1688480877743_manager-tables/down.sql
+++ hasura-badges/migrations/default/1688480877743_manager-tables/down.sql
@@ -1,1 +1,2 @@
-SELECT NOW();
+DROP TABLE IF EXISTS "engineer_badge_candidature_proposals";
+DROP FUNCTION IF EXISTS "get_engineers_by_manager"(INTEGER);
--- hasura-badges/migrations/default/1688480877743_manager-tables/up.sql
+++ hasura-badges/migrations/default/1688480877743_manager-tables/up.sql
@@ -1,3 +1,15 @@
+
+CREATE TABLE engineer_badge_candidature_proposals (
+  engineer INTEGER NOT NULL REFERENCES users(id),
+  badge_id INTEGER NOT NULL,
+  badge_version TIMESTAMP NOT NULL,
+  proposal_description VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  created_by INTEGER REFERENCES users(id) ON DELETE RESTRICT,
+  PRIMARY KEY (engineer, badge_id, badge_version),
+  FOREIGN KEY (badge_id, badge_version) REFERENCES badges_versions(id, created_at)
+);
+
 CREATE OR REPLACE FUNCTION get_engineers_by_manager(manager_id INTEGER)
   RETURNS SETOF public.users AS
 $$
