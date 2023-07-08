0535ad9e16b8842ec7447105d3fcba9530642a5c
Frajdi <80971287+Frajdi@users.noreply.github.com>
2023-07-08 07:07:14 +0000

    some more touches : }

--- hasura-badges/migrations/default/1688480877743_manager/down.sql
+++ hasura-badges/migrations/default/1688480877743_manager/down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS "engineer_badge_candidature_proposals";
-DROP TABLE IF EXISTS "engineer_badge_candidature_response";
-DROP FUNCTION IF EXISTS "get_engineers_by_manager"(INTEGER);
-DROP FUNCTION IF EXISTS "get_approved_responses_by_manager"(INTEGER);
+DROP FUNCTION IF EXISTS "get_engineers_by_manager";
+DROP FUNCTION IF EXISTS "get_approved_responses_by_manager";
+DROP TABLE IF EXISTS "engineer_badge_candidature_proposal_response";
+DROP TABLE IF EXISTS "manager_to_engineer_badge_candidature_proposals";
