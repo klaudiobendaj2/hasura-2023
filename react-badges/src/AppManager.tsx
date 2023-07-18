4a43567a9358f29044af450a6ff37cceb74d44ed
ledjoshukulli <71833239+ledjoshukulli@users.noreply.github.com>
2023-07-18 12:27:39 +0000

    loginView

--- react-badges/src/AppManager.tsx
+++ react-badges/src/AppManager.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import AppEntrypoint, { ManagerIcon } from "./containers/AppEntrypoint";
 import AssociatedEngineers from "./associated engineers/AssociatedEngineers";
 import { DrawerMenu } from "./layouts/BasicLayout";
@@ -9,6 +9,9 @@ import GroupWorkIcon from '@mui/icons-material/GroupWork';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import RuleIcon from '@mui/icons-material/Rule';
 import ApprovalRejectionIssues from "./Managers/ApprovalRejectionIssues";
+import './App.css';
+import { AuthContext } from "./state/with-auth";
+import { users } from "./views/LoginView";
 
 const managerMenuItems = [
   {
@@ -33,51 +36,54 @@ const managerMenuItems = [
   }
 ];
 
+const AppManager: React.FC = () => {
+  const { managerId } = useContext(AuthContext);
+  const manager = users.find((user) => user.id === managerId);
+  console.log('managerid', typeof managerId);
 
-const AppManager: React.FC = () => (
-  <AppEntrypoint
-    icon={<ManagerIcon />}
-    title="Manager"
-    defaultRoute="managers/badges"
-    drawerContents={[<DrawerMenu title="Manager:" items={managerMenuItems} />]}
-    mobileUtils={managerMenuItems}
-    routes={[
-      {
-        path: "managers/AssociatedEngineers",
-        element: (
-          <div>
-            <AssociatedEngineers />
-          </div>
-        )
-      },
-      {
-        path: "managers/badges",
-        element: (
-          <div>
-            <AvailableBadges/>
-          </div>
-        )
-      },
-      {
-        path: "managers/CandidatureProposals",
-        element: (
-          <div>
-            <CandidatureProposals/>
-          </div>
-        )
-      },
-      {
-        path: "managers/IssuingRequest",
-        element: (
-          <div>
-            <ApprovalRejectionIssues/>
-          </div>
-        )
-      }
-    ]}
-    
-  />
-);
+  return (
+    <AppEntrypoint
+      icon={<ManagerIcon/>}
+      title={`${manager?.name} (Manager)`}
+      defaultRoute="managers/badges"
+      drawerContents={[<DrawerMenu title="Manager:" items={managerMenuItems} />]}
+      mobileUtils={managerMenuItems}
+      routes={[
+        {
+          path: "managers/AssociatedEngineers",
+          element: (
+            <div>
+              <AssociatedEngineers />
+            </div>
+          )
+        },
+        {
+          path: "managers/badges",
+          element: (
+            <div>
+              <AvailableBadges/>
+            </div>
+          )
+        },
+        {
+          path: "managers/CandidatureProposals",
+          element: (
+            <div>
+              <CandidatureProposals/>
+            </div>
+          )
+        },
+        {
+          path: "managers/IssuingRequest",
+          element: (
+            <div>
+              <ApprovalRejectionIssues/>
+            </div>
+          )
+        }
+      ]}
+    />
+  );
+};
 
 export default AppManager;
-
