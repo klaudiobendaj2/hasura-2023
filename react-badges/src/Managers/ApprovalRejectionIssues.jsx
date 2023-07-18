5f5c409cf631f5fa64751313808c6d3d9e16e275
kamelamehmet <109985715+kamelamehmet@users.noreply.github.com>
2023-07-18 14:34:41 +0000

    style for issue requests

--- react-badges/src/Managers/ApprovalRejectionIssues.jsx
+++ react-badges/src/Managers/ApprovalRejectionIssues.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { useContext } from "react";
 import { AuthContext } from "../state/with-auth";
-import {  Button, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import {
   GET_ISSUING_REQUESTS,
   REJECT_ISSUING_REQUEST,
   APPROVE_ISSUING_REQUEST
 } from "../state/queries-mutations.graphql";
-import Swal from "sweetalert2";
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import Typography from '@mui/material/Typography';
-
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 const ApprovalRejectionIssues = () => {
   const [showRejectionTextArea, setShowRejectionTextArea] = useState(false);
@@ -27,11 +27,13 @@ const ApprovalRejectionIssues = () => {
   const [approveIssuingRequest] = useMutation(APPROVE_ISSUING_REQUEST);
 
   const handleApprovalClick = async (id) => {
-    Swal.fire(
-      'The issue request was approved!',
-      '',
-      'success'
-    )
+    // alert(
+    //   'The issue request was approved!',
+    //   '',
+    //   'success'
+    // )
+    setShowRejectionTextArea(false);
+
     try {
       await approveIssuingRequest({
         variables: {
@@ -58,9 +60,7 @@ const ApprovalRejectionIssues = () => {
   };
 
   const handleRejectionSubmit = async () => {
-    Swal.fire(
-      'The issue request was not approved!'
-    )
+    alert("The issue request was not approved!");
     try {
       await rejectIssuingRequest({
         variables: {
@@ -97,43 +97,65 @@ const ApprovalRejectionIssues = () => {
       </Typography>
       {data && data.get_issuing_requests_for_manager.length === 0 ? (
         <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        <strong>No issue requests found!</strong>
-      </Alert>
+          <AlertTitle>Info</AlertTitle>
+          <strong>No issue requests found!</strong>
+        </Alert>
       ) : (
         data &&
         data.get_issuing_requests_for_manager.map((issue) => (
-          <div key={issue.id}>
-            <h3>{issue.badge_title}</h3>
-            <p>{issue.badge_description}</p>
-            <p>
-              <strong>Badge Version: </strong>
-              {issue.badge_version}
-            </p>
-            <p>
-              <strong>Engineer: </strong>
-              {issue.engineer_name}
-            </p>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => handleApprovalClick(issue.id)}
-              sx={{ marginRight: "10px" }}
-            >
-              Approve
-            </Button>
-
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => handleRejectionClick(issue.id)}
-              sx={{ backgroundColor: "red" }}
-            >
-              Reject
-            </Button>
-
+          <Card
+            key={issue.id}
+            variant="outlined"
+            sx={{
+              marginBottom: "10px",
+              width: "677px",
+              marginLeft: "280px",
+              height: showRejectionTextArea ? "550px" : "350px",
+              backgroundColor: " #F1F6F9"
+            }}
+          >
+            <CardContent>
+              <Typography
+                display="flex"
+                justifyContent="center"
+                variant="h5"
+                component="h3"
+                gutterBottom
+              >
+                <strong>{issue.badge_title}</strong>
+              </Typography>
+              <Typography textAlign="justify" variant="body1" component="p">
+                {issue.badge_description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <strong>Badge Version: </strong>
+                {issue.badge_version}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <strong>Engineer: </strong>
+                {issue.engineer_name}
+              </Typography>
+            </CardContent>
+            <div className="issuebuttons">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleApprovalClick(issue.id)}
+                sx={{ marginRight: "10px" }}
+              >
+                Approve
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => handleRejectionClick(issue.id)}
+                sx={{ backgroundColor: "red" }}
+              >
+                Reject
+              </Button>
+            </div>
             {showRejectionTextArea && (
-              <div>
+              <CardContent>
                 <TextField
                   value={rejectionDescription}
                   onChange={handleDescriptionChange}
@@ -143,7 +165,6 @@ const ApprovalRejectionIssues = () => {
                   variant="outlined"
                   fullWidth
                 />
-
                 <Button
                   variant="contained"
                   color="secondary"
@@ -152,9 +173,9 @@ const ApprovalRejectionIssues = () => {
                 >
                   Submit
                 </Button>
-              </div>
+              </CardContent>
             )}
-          </div>
+          </Card>
         ))
       )}
     </div>
