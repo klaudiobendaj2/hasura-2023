0aa638efa891147b4976b4264ce8fdca4fb42652
klaudiobendaj2 <139866544+klaudiobendaj2@users.noreply.github.com>
2023-07-19 14:00:40 +0000

    Merging from main

--- react-badges/src/components/CandidatureProposal/AddCandidatureProposal.jsx
+++ react-badges/src/components/CandidatureProposal/AddCandidatureProposal.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { AuthContext } from "../../state/with-auth";
 import {
