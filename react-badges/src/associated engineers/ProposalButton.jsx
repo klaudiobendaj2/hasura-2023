9816749466c34dd688e9f4c9c0f19052ef53c03b
ledjoshukulli13 <139954849+ledjoshukulli13@users.noreply.github.com>
2023-07-20 14:13:35 +0000

    engineersChange

--- react-badges/src/associated engineers/ProposalButton.jsx
+++ react-badges/src/associated engineers/ProposalButton.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Button } from '@mui/material';
-import './Button.css';
 
 function ProposalButton({ onClick, engineer }) {
   const handleButtonClick = () => {
     onClick(engineer);
   };
   return (
-    <Button className='proposalButton' variant="contained" onClick={handleButtonClick}>
-      +
+    <Button className='proposalButton' sx={{fontSize:"10px"}} variant="contained" onClick={handleButtonClick}>
+      Add Proposal
     </Button>
   );
 }
